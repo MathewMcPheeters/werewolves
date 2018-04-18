f56b8c9b3f560ff8bc72a0f111b388c14b1b8cda
Mathew <mmcpheeters@unm.edu>
2018-04-17 23:01:11 -0600

    upload

--- update.sh
+++ update.sh
@@ -4,3 +4,9 @@ sudo chown $1 /home/$1/client.py
 sudo chgrp $1 /home/$1/client.py
 sudo chmod 740 /home/$1/client.py
 sudo rm /home/$1/.client.py.swp
+
+sudo cp communication.py /home/$1/communication.py
+sudo chown $1 /home/$1/communication.py
+sudo chgrp $1 /home/$1/communication.py
+sudo chmod 740 /home/$1/communication.py
+sudo rm /home/$1/.communication.py.swp
