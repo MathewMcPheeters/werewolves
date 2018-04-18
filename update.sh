baab1eb12316ddf45548e2dfed9449f66c7c8320
Mathew <mmcpheeters@unm.edu>
2018-04-17 22:58:28 -0600

    upload

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 echo 'updating '$1
 sudo cp client.py /home/$1/client.py
-sudo chown group$1 /home/$1/client.py
-sudo chgrp group$1 /home/$1/client.py
+sudo chown $1 /home/$1/client.py
+sudo chgrp $1 /home/$1/client.py
 sudo chmod 740 /home/$1/client.py
 sudo rm /home/$1/.client.py.swp
