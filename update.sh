3cf10b0338170ab1a85bafea57c946bae46898d7
Mathew <mmcpheeters@unm.edu>
2018-04-19 11:40:39 -0600

    upload

--- update.sh
+++ update.sh
@@ -5,6 +5,12 @@ sudo chgrp $1 /home/$1/client.py
 sudo chmod 740 /home/$1/client.py
 sudo rm /home/$1/.client.py.swp
 
+sudo cp client_hacked.py /home/$1/client.py
+sudo chown $1 /home/$1/client_hacked.py
+sudo chgrp $1 /home/$1/client_hacked.py
+sudo chmod 740 /home/$1/client_hacked.py
+sudo rm /home/$1/.client_hacked.py.swp
+
 sudo cp communication.py /home/$1/communication.py
 sudo chown $1 /home/$1/communication.py
 sudo chgrp $1 /home/$1/communication.py
